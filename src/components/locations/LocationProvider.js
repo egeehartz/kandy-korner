3d7bf6abd610d156ac05b84459bfa322d586ffa5
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-09-03 13:09:38 -0500

    wrote out provider and location.js

---
+++ src/components/locations/LocationProvider.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react"
+
+
+ //The context is imported and used by individual components that need data
+export const LocationContext = React.createContext()
+
+export const LocationProvider = (props) => {
+    const [locations, setLocations] = useState([])
+
+    const getLocations = () => {
+        return fetch("http://localhost:8088/locations")
+            .then(res => res.json())
+            .then(setLocations)
+    }
+
+    const addLocation = location => {
+        return fetch("http://localhost:8088/locations", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(location)
+        })
+            .then(getLocations)
+    }
+
+    return (
+        <LocationContext.Provider value={{
+            locations, addLocation, getLocations
+        }}>
+            {props.children}
+        </LocationContext.Provider>
+    )
+}
