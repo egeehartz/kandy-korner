2c8a835ae3f29dfcacd0105fdf2b97cf0e2238de
Emily Hartzell <emily.g.hartzell@gmail.com>
2020-09-03 13:29:35 -0500

    locations rendered with correct properties

--- src/components/locations/Location.js
+++ src/components/locations/Location.js
@@ -3,7 +3,7 @@ import "./Location.css"
 
 export const Location = ({ location }) => (
     <section className="location">
-        <h3 className="location__name">{location.name}</h3>
-        <div className="location__place">{location.place}</div>
+        <h3 className="location__place">{location.address}</h3>
+        <div className="location__sqft">{location.sqft} square feet</div>
     </section>
 )
